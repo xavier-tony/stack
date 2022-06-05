a8a428113ac8b36cac24ff713a60064bb8426e79
Tony <tonyxavier@live.com>
2022-06-04 22:46:57 -0400

    feat: add ngrx

--- apps/out-school/src/app/app.module.ts
+++ apps/out-school/src/app/app.module.ts
@@ -2,13 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { NxWelcomeComponent, AppComponent, environment } from '@stack/out-school';
+import {
+  NxWelcomeComponent,
+  AppComponent,
+  environment,
+} from '@stack/out-school';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ENVIRONMENT } from '@stack/services';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
   providers: [
     {
       provide: ENVIRONMENT,
